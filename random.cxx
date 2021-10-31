bf0c9fd1d53c68da2977f1cd884c3ac48bb07dd3
Daniel Stubbs <dnl.stubbs@gmail.com>
2021-10-31 09:02:42 -0400

    Replaced a reference to NULL by the more appropriate nullptr.

--- random.cxx
+++ random.cxx
@@ -2,9 +2,10 @@
 
 Random::Random()
 {
+  unsigned long s = std::time(nullptr);
   gen = new std::mt19937(rd());
   VRG = new std::uniform_real_distribution<>(0.0,1.0);
-  gen->seed(long(std::time(NULL)));
+  gen->seed(s);
 }
 
 Random::~Random()
@@ -13,9 +14,9 @@ Random::~Random()
   delete VRG;
 }
 
-void Random::initialize_generator(unsigned long seed)
+void Random::initialize_generator(unsigned long s)
 {
-  gen->seed(seed);
+  gen->seed(s);
 }
 
 int Random::irandom(int nmax)
